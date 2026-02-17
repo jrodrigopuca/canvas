962e426e25c0f13be20ef54a0b6b984eb78a8865
jarp <jarp@outlook.com>
2026-02-17 13:47:32 -0300

    fix: correct indentation in vite.config.ts for better readability

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig(({ command }) => {
 							insertTypesEntry: true,
 							rollupTypes: true,
 						}),
-				  ]
+					]
 				: []),
 		],
 		// Library build configuration
